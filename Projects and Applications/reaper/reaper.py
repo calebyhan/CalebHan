648e4ed358b36e3f11927659aa9056a7e1cb4cd8
Caleb Han <59373603+calebyhan@users.noreply.github.com>
2020-09-16 21:10:22 -0400

    Update reaper.py

--- Projects and Applications/reaper/reaper.py
+++ Projects and Applications/reaper/reaper.py
@@ -3,11 +3,11 @@ Date: July 2020
 Details: Used to format raw output to useful data
 """
 
-#imports
 import re
 import sys
 
 #multi-line input
+
 x = sys.stdin.readlines()
 #running = True
 list1 = []
@@ -21,14 +21,19 @@ for item in x:
         r = re.search(r'(?<=raven.min.js:2 )[.\-\w= :&]+',item).group(0)
         list1.append(r)
 
-#processes this line by line
+#processes the data line by line
+
 for item in list1:
     #print(item)
-    #checks if the line is valid
+    
+    #checks if the line is valid for regex
+    
     if item[0] in ("0","1","2","3","4","5","6","7","8","9", "U"):
         item = item
     else:
+        
         #print(item)
+        
         time = re.search(r'(?<=time=)[a-zA-Z0-9: ]+', item).group(0)
         username = re.search(r'(?<=user=)[\w\- ]+', item).group(0)
         raw = re.search(r'(?<=raw=)[0-9:]+', item).group(0)
@@ -54,5 +59,7 @@ for item in list1:
             actual="12:{}:0{}AM".format(hour, minute)
         else:
             actual="12:{}:{}AM".format(hour, minute)
+            
         #final output
+        
         print("Time: {} Username: {} Raw: {} Actual: {} Bonus: {}".format(time, username, raw, actual, bonus))

074236e0c764ea821f5f801d04716df0fa449aac
Caleb Han <calebhantech@gmail.com>
2022-09-05 11:47:56 -0400

    added Chapter 1 files

---
+++ Projects and Applications/HMCA_Labs/Chapter 1/Errors.java
@@ -0,0 +1,14 @@
+/*
+ * Name: Caleb Han
+ * Description: editing errors
+*/
+
+public class Errors {
+	public static void main(String[] args) {
+		System.out.println("I enjoy computer science!");
+		int x = 5;
+		double y = 4.0;
+		double num = (int) x + y;
+		System.out.println("x + y = " + num);
+	}
+}
---
+++ Projects and Applications/HMCA_Labs/Chapter 1/Intro.java
@@ -0,0 +1,13 @@
+/*
+ * Name: Caleb Han
+ * Description: printing a heart
+*/
+
+public class Intro {
+	public static void main(String[] args) {
+		System.out.println("/-\\/-\\");
+		System.out.println("\\----/");
+		System.out.println(" \\--/");
+		System.out.println("  \\/");
+	}
+}

eb062a029d734d2a436014804737badea31af9df
calebyhan <calebhantech@gmail.com>
2024-07-19 09:44:08 -0400

    commit

--- Competitive Programming/LeetCode/36.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
-        try:
-            return nums.index(target)
-        except:
-            for i in range(len(nums)):
-                if nums[i] > target:
-                    return i
-            return len(nums)
